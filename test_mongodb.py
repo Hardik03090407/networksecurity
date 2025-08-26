d32d3ff5e857e091997cdbb94322f3f16204b60e
Hardik03090407 <hardikrawat79@gmail.com>
2025-08-27 01:31:51 +0530

    ETP Pipelines completed

--- test_mongodb.py
+++ test_mongodb.py
@@ -1,7 +1,7 @@
 
 from pymongo.mongo_client import MongoClient
 
-uri = "mongodb+srv://hardikrawat79:03090407HS@cluster0.a0kvsxv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://hardikrawat79:1234567890@cluster0.a0kvsxv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri)
